3602873f6bec9f04cd2abfbfb33a7bba9e3fb4a3
manu12perez <manu12perez@gmail.com>
2025-02-19 12:50:25 +0100

    @Html.AntiForgeryToken() [ValidateAntiForgeryToken] builder.Services.AddAntiforgery();

--- MvcNetCoreCSRF/Program.cs
+++ MvcNetCoreCSRF/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAntiforgery();
 builder.Services.AddControllersWithViews();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
